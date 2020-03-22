0fca49434ae22670c3b3dab5f66cedd2ef8a303f
MadhuVS <madhuvs.in@gmail.com>
2020-03-22 10:06:54 -0500

    inital setup

---
+++ src/app.js
@@ -0,0 +1,36 @@
+const http = require('http');
+const express = require('express');
+const accountSid = 'AC4afc9a4ad304cdd41a29d6c113c65696';
+const authToken = 'cb475a819a809ccd777e06a9f87fed39';
+const client = require('twilio')(accountSid, authToken);
+const MessagingResponse = require('twilio').twiml.MessagingResponse;
+
+const app = express();
+
+app.get('/hearbeat',(req,res) =>{
+  res.send("Alive");
+})
+
+app.post('/sms', (req, res) => {
+  console.log("message received");
+  const twiml = new MessagingResponse();
+  twiml.message('There is a pink mermaid - Sana');
+
+  res.writeHead(200, {'Content-Type': 'text/xml'});
+  res.end(twiml.toString());
+});
+
+
+client.messages
+  .create({
+     body: 'This is the ship that made the Kessel Run in fourteen parsecs?',
+     from: '+12084875541',
+     to: '+15558675310'
+   })
+  .then(message => console.log(message.sid)); 
+
+http.createServer(app).listen(1337, () => {
+  console.log('Express server listening on port 1337');
+});
+
+
---
+++ src/send_sms.js
@@ -0,0 +1,14 @@
+// Download the helper library from https://www.twilio.com/docs/node/install
+// Your Account Sid and Auth Token from twilio.com/console
+// DANGER! This is insecure. See http://twil.io/secure
+const accountSid = 'AC4afc9a4ad304cdd41a29d6c113c65696';
+const authToken = 'your_auth_token';
+const client = require('twilio')(accountSid, authToken);
+
+client.messages
+  .create({
+     body: 'This is the ship that made the Kessel Run in fourteen parsecs?',
+     from: '+15017122661',
+     to: '+15558675310'
+   })
+  .then(message => console.log(message.sid));
